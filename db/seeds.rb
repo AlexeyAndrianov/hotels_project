f9e451177a99c2ce7178a9df04da8cbdb9bbb611
Alexey <alexey.andrianov@oxagile.com>
2018-06-26 12:28:55 +0300

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Hotel.create(name: "Awesome Hotel", adress: "USA Minnesota", description: "super")
+Hotel.create(name: "bad Hotel", adress: "USA Texas", description: "awfull")
+Feedback.create(send_by: "Adam Smith",
+ review: "nice",
+ cleanliness: 5,
+ placement: 5,
+ communication: 5,
+ hotel_id: 1)
+Feedback.create(send_by: "Alex",
+ review: "awesome!",
+ cleanliness: 5,
+ placement: 4,
+ communication: 5,
+ hotel_id: 2)
+Feedback.create(send_by: "Samuel",
+ review: "Not bad!",
+ cleanliness: 4,
+ placement: 4,
+ communication: 5,
+ hotel_id: 3)
+Feedback.create(send_by: "Dave",
+ review: "awesome!",
+ cleanliness: 2,
+ placement: 1,
+ communication: 1,
+ hotel_id: 4)
