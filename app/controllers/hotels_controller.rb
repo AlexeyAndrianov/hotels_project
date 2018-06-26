d7eec42d1ba670be32b02df0370d924eb05a24ac
Alexey <alexey.andrianov@oxagile.com>
2018-06-26 16:25:16 +0300

    average statistics

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -7,5 +7,12 @@ class HotelsController < ApplicationController
     @hotel = Hotel.find(params[:id])
     @feedback = @hotel.feedbacks.build
     @persisted_feedbacks = @hotel.feedbacks.find_all  { |feedback| feedback.persisted? }
+    @average_rating_cleanliness =
+      (@persisted_feedbacks.sum(&:cleanliness).to_f /
+      @persisted_feedbacks.count).round(2)
+    @average_rating_placement = (@persisted_feedbacks.sum(&:placement).to_f /
+      @persisted_feedbacks.count).round(2)
+    @average_rating_comunication = (@persisted_feedbacks.sum(&:communication).to_f /
+      @persisted_feedbacks.count).round(2)
   end
 end
