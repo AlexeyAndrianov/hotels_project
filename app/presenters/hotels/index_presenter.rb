962e1c33ab7cb0f1d28f69eba21948bf9a43b857
Alexey <alexey.andrianov@oxagile.com>
2018-06-27 16:52:08 +0300

    define_method refactoring

--- app/presenters/hotels/index_presenter.rb
+++ app/presenters/hotels/index_presenter.rb
@@ -7,16 +7,12 @@ module Hotels
       @hotel = hotel
       @persisted_feedbacks = hotel.feedbacks.find_all  { |feedback| feedback.persisted? }
     end
-    def average_rating_cleanliness
-      @hotel.feedbacks.average(:cleanliness).round(2)
-    end
 
-    def average_rating_placement
-      @hotel.feedbacks.average(:placement).round(2)
-    end
-
-    def average_rating_comunication
-      @hotel.feedbacks.average(:communication).round(2)
+    [:cleanliness, :placement, :communication].each do |name|
+      define_method(name) do
+        @hotel.feedbacks.average(name).round(2)
+      end
     end
   end
+
 end
